0fa39bf8ab9cfa5ef0783f50d4322d3f22c97e18
naufalmfayiz <fayiznaufal@gmail.com>
2024-12-08 15:31:13 +0700

    enable cors

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const express = require("express");
 const errorHandler = require("./middlewares/errorHandler");
 const app = express();
 const port = 3000;
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.json());
 
 app.use(require("./routes/index"));
